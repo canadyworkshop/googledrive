8ebdef1b6daac68599a050dcc1981e916cfdd530
Jeremy Canady <git.jeremy@canady.family>
2023-08-16 00:50:36 +0000

    wip

--- cmd/file.list.go
+++ cmd/file.list.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/googleapi"
 	"os"
 	"text/tabwriter"
 )
@@ -18,6 +22,7 @@ var FileListCmdVars = struct {
 	paged          bool
 	pageToken      string
 	listLongFormat bool
+	outFormat      string
 }{}
 
 var fileListCmd = &cobra.Command{
@@ -40,11 +45,14 @@ var fileListCmd = &cobra.Command{
 		}
 
 		if FileListCmdVars.fields != "" {
-			call = call.DriveId(FileListCmdVars.fields)
+			fieldsQuery := fmt.Sprintf("files(%s)", FileListCmdVars.fields)
+			call = call.Fields(googleapi.Field(fieldsQuery))
+		} else {
+			call.Fields(googleapi.Field("files(*)"))
 		}
 
 		if FileListCmdVars.includeLabels != "" {
-			call = call.DriveId(FileListCmdVars.includeLabels)
+			call = call.IncludeLabels(FileListCmdVars.includeLabels)
 		}
 
 		if FileListCmdVars.orderBy != "" {
@@ -55,28 +63,44 @@ var fileListCmd = &cobra.Command{
 			call = call.PageSize(FileListCmdVars.pageSize)
 		}
 
-		if FileListCmdVars.paged && FileListCmdVars.pageToken != "" {
-			call = call.PageToken(FileListCmdVars.pageToken)
-		}
+		// Handle paged requests.
+		var files []*drive.File
 
-		fileList, err := call.Do()
-		if err != nil {
-			log.Error(err.Error())
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		if FileListCmdVars.listLongFormat {
-			w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
-			for _, f := range fileList.Files {
-				fmt.Fprintf(w, "%s\t%s\t%d\t%s\t\n", f.Id, f.Name, f.Size, f.MimeType)
-
+		if FileListCmdVars.paged {
+			if FileListCmdVars.pageToken != "" {
+				call.PageToken(FileListCmdVars.pageToken)
 			}
-			w.Flush()
+
+			fileList, err := call.Do()
+			if err != nil {
+				log.Error(err.Error())
+				os.Exit(1)
+			}
+			files = fileList.Files
 		} else {
-			for _, f := range fileList.Files {
-				fmt.Println(f.Name)
+			err := call.Pages(context.Background(), func(list *drive.FileList) error {
+				fmt.Println("paged")
+				files = append(files, list.Files...)
+				return nil
+			})
+			//fileList, err := call.Do()
+			if err != nil {
+				log.Error(err.Error())
+				os.Exit(1)
 			}
+		}
+
+		switch FileListCmdVars.outFormat {
+
+		case "json":
+			json, err := json.MarshalIndent(files, "", "  ")
+			if err != nil {
+				log.Error(err.Error())
+				os.Exit(1)
+			}
+			fmt.Println(string(json))
+		default:
+			outputFilesStd(files)
 		}
 
 	},
@@ -95,4 +119,26 @@ func init() {
 	fileListCmd.Flags().BoolVar(&FileListCmdVars.paged, "paged", false, "Enables paged results.")
 	fileListCmd.Flags().StringVar(&FileListCmdVars.pageToken, "page-token", "", "The token for the next page when using the paged option.")
 	fileListCmd.Flags().BoolVarP(&FileListCmdVars.listLongFormat, "list", "l", false, "List files in long format.")
+	fileListCmd.Flags().StringVar(&FileListCmdVars.outFormat, "out", "std", "List files in long format.")
+
+}
+
+func outputFilesStd(files []*drive.File) {
+	if FileListCmdVars.listLongFormat {
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+		for _, f := range files {
+			if _, err := fmt.Fprintf(w, "%s\t%s\t%d\t%s\t\n", f.Id, f.Name, f.Size, f.MimeType); err != nil {
+				log.Error(err.Error())
+				os.Exit(1)
+			}
+		}
+		if err := w.Flush(); err != nil {
+			log.Error(err.Error())
+			os.Exit(1)
+		}
+	} else {
+		for _, f := range files {
+			fmt.Println(f.Name)
+		}
+	}
 }
